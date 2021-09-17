6a5d68ab39ead504a1e105fdf4f096a91e5cce7d
ilexwg <ilexwg@gmail.com>
2021-09-17 15:27:54 +0800

    add config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,26 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    '@vue/prettier',
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'es5',
+        arrowParens: 'always',
       },
-    },
-  ],
+    ],
+  },
 };
